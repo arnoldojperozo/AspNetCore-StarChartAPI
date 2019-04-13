0c20b61b468c50880d7cdf39f5fdd14dd385cc4d
Arnoldo Perozo <arnoldo.perozo@hotmail.com>
2019-04-13 16:48:07 -0600

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using System.Security.AccessControl;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -13,6 +15,22 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var returnValue = _context.CelestialObjects.Where(x => x.Id == id)
+                .Include(y=>y.Satellites);
+
+            if (returnValue != null)
+            {
+                return Ok(returnValue);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
